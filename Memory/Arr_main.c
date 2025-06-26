3e9faafc23d06b34b0caf96d0d90e2a77fe9f8ce
sachithraperera255 <sachithraperera255@gmail.com>
2025-06-25 22:01:29 -0400

    Initial commit - Array subscript operator

---
+++ Memory/Arr_main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char* argv[])
+{
+    int arr[] = {5, 13, 22};
+
+    /*
+        The conventional method to access a array is as follows arr[0], which would give
+        access to the fist element in the array but what actually happens in background is
+        that we calls the array as it declared, which gives the address of the array, then
+        use * to dereference to access the values as hows below.
+    */
+
+    printf("%d\n", *(arr + 1)); // OUTPUT: 13
+
+    /*
+        Due to the fact that addition is commutative, A + B = c or B + A = C, following method
+        can be used to get the results,
+
+        printf("%d\n", 1[arr]); OUTPUT: 13
+    */
+
+
+    return 0;
+}
