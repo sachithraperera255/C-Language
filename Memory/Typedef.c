6356e50a4d70b750cac5be6e38a57b9bc36484c9
sachithraperera255 <sachithraperera255@gmail.com>
2025-07-11 13:12:39 -0400

    why use typedef

---
+++ Memory/Typedef.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+    What is typedef --> typedef let we create a nickname for a data type. it's purely a syntactic
+    convenience. consider the following examples.
+*/
+
+typedef unsigned long long UINT64;
+
+typedef struct Point
+{
+    double x,y;
+}Point;
+
+// Another way of declaring the struct
+
+// struct Point
+// {
+//     double x,y;
+// };
+
+int main (int argc, char* argv[])
+{
+    //struct Point p;
+    Point p;
+    p.x = 0.24;
+    p.y = 0.78;
+    printf("%lf %lf\n", p.x, p.y);
+
+    UINT64 l = 14;
+    printf("%llu\n", l);
+
+    return 0;
+}
