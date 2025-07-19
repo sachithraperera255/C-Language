f9cac83f0e6825e7a948fb0751f2a5a0b933d1a7
sachithraperera255 <sachithraperera255@gmail.com>
2025-07-19 00:07:56 -0400

    What is enum in C

---
+++ Memory/Enum.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/*
+    - An enumerated type is a user defined type which is used to assign names to integral 
+    constant because names are easier to handle in program.
+    - If we don't assign values to enum then, compiler will assign integer values to them starting from 0 or
+    values of previous name + 1;
+
+    - Why do we need enum:
+        - Enum can be declared in the local scope --> #define (macro) only in the global scope.
+        - Enum are automatically assigned by the compiler.
+        - Two or more names can have the same value.
+*/
+
+
+typedef enum Color {
+    RED = 7,
+    BLUE,
+    YELLOW
+}Color;
+
+
+int main (int argc, char* argv[])
+{
+    Color c = RED;
+    printf("%d\n", c);
+    return 0;
+}
