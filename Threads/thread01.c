c0f5856206ede3cf3780270d03f3a20633cd043e
sachithraperera255 <sachithraperera255@gmail.com>
2026-03-14 23:37:43 -0400

    Introduction to threads

--- Threads/thread01.c
+++ Threads/thread01.c
@@ -7,14 +7,28 @@
 
 
 // This function is called thread routine and it will be executed by thread
+// This function will return a void pointer 
 void* routine(){
     printf("Testing Threads");
 }
 
 int main(int argc, char* argv[])
-{
+{   
+    // t1 will store thread ID. After pthread_create() function is executed t1 will have ID of the newly 
+    //created thread
     pthread_t t1;
+
+    /*
+        - following function create the thread, which tells the OS that create another execution path
+        (thread) inside this process and start running respective function.
+        - If the function return non-zero value --> error
+    */
     pthread_create(&t1, NULL, &routine, NULL);
+
+    /*
+        - pthread_join() function tells main function to wait until t1 finishes. If the main function exit
+        before thread finishes, t1 will terminate before its return the value.
+    */
     pthread_join(t1, NULL);
     return 0;
 }
