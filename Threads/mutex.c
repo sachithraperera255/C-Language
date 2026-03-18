4da8a11469ab4d7fa11dafa8e065fdb348a881bd
sachithraperera255 <sachithraperera255@gmail.com>
2026-03-17 21:49:13 -0400

    Methods to prevent race condition

--- Threads/mutex.c
+++ Threads/mutex.c
@@ -9,11 +9,26 @@ int mails = 0;
     - What is a rece condition?
         - Race condition occurs when multiple threads trying access or modify shared data at the 
         same time. this could lead to unpredictable results.
+
+    - Ways to prevent and handle the race condition:
+        - Locks/Mutex (Mutula exclusion): this method make sure that only one thread is 
+        allow inside the critical section at a time.
+        - Semaphore: This method is synchronization tool that controls how many threads can access
+        a resource at the same time.
+            - There are two main types:
+                - Binary semaphores
+                - Counting semaphores
+        - Atomic operations: This method is an operation that execute completely or not at all
+        without been interupted from other threads
+*/
+
+/*
+    - 
 */
 
 void* routine()
 {
-    for (int i = 0; i < 1000000; i++)
+    for (int i = 0; i < 100000; i++)
     {
         mails++;
     }
