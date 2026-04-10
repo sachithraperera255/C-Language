3e55a1b3daf941d818d805004e9d64534ef5065a
sachithraperera255 <sachithraperera255@gmail.com>
2026-04-09 22:19:34 -0400

    pthread_cond_init/wait/signal

--- Threads/conditionVariable.c
+++ Threads/conditionVariable.c
@@ -3,44 +3,78 @@
 #include <unistd.h>
 #include <pthread.h>
 
-void* fuel_filling(void* arg)
-{
+// Creating a mutex
+pthread_mutex_t mute;
+// Creating a condition variable
+pthread_cond_t fuelCond;
 
+int fuel = 0;
+
+void *fuel_filling(void *arg)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        pthread_mutex_lock(&mute);
+        fuel += 15;
+        printf("Filled fuel... %d\n", fuel);
+        pthread_mutex_unlock(&mute);
+        pthread_cond_signal(&fuelCond);
+        sleep(1);
+    }
 }
 
-void* car(void* arg)
+void *car(void *arg)
 {
-
+    pthread_mutex_lock(&mute);
+    while (fuel < 40)
+    {
+        printf("No fuel, waiting\n");
+        // Waiting for a signal from another thread
+        pthread_cond_wait(&fuelCond, &mute);
+    }
+    
+    fuel -= 40;
+    printf("Got fuel. Now left: %d\n", fuel);
+    pthread_mutex_unlock(&mute);
 }
 
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     pthread_t th[2];
+    pthread_mutex_init(&mute, NULL);
+    pthread_cond_init(&fuelCond, NULL);
 
-    for(int i = 0; i < 2; i++)
+    for (int i = 0; i < 2; i++)
     {
-        if (pthread_create(&th[i], NULL, &fuel_filling, NULL) != 0)
+        if (i == 1)
         {
-            perror("Failed to create threads\n");
 
-        }else
+            if (pthread_create(&th[i], NULL, &fuel_filling, NULL) != 0)
+            {
+                perror("Failed to create threads\n");
+            }
+        }
+        else
         {
             if (pthread_create(&th[i], NULL, &car, NULL) != 0)
             {
                 perror("Failed to create thread\n");
             }
-            
         }
-        
     }
 
-    for(int i = 0; i < 2; i++)
+    for (int i = 0; i < 2; i++)
     {
-        if(pthread_join(th[i], NULL) != 0)
+        if (pthread_join(th[i], NULL) != 0)
         {
             printf("Thread failed to join\n");
         }
     }
+
+    // Destroy the mutex
+    pthread_mutex_destroy(&mute);
+    // Destroy the condition variable
+    pthread_cond_destroy(&fuelCond);
 
     return 0;
 }
