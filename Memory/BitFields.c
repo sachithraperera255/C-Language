bc2858b06e8040de4f74b33f5d33a4c809469361
sachithraperera255 <sachithraperera255@gmail.com>
2026-05-03 23:22:49 -0400

    decoding the data packet using bit-field

---
+++ Memory/BitFields.c
@@ -0,0 +1,59 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdint.h>
+
+/*
+    - In Embedded C, bit fields are a way to divide variables into smaller groups. Mostly used to
+    read packet data.
+*/
+
+// The following struct represent bit-fields of a data packet.
+struct Packets
+{
+    uint8_t crc:        2;
+    uint8_t status:     1;
+    uint16_t payload:   12;
+    uint8_t bat:        3;
+    uint8_t sensor:     3;
+    uint8_t longAddr:   8;
+    uint8_t shortAddr:  2;
+    uint8_t addrMode:   1;
+};
+
+int main(void)
+{
+    // This variable is created to save the data packet.
+    uint32_t readVal;
+
+    // Reading the data packet
+    printf("Enter the 32bits packet here: ");
+    scanf("%X", &readVal);
+
+    // cleating a instance of a structure.
+    struct Packets packet;
+
+    // Decoding and grouping the bit-fields in the data packet
+    packet.crc = (uint8_t)(readVal & 0x3);
+    packet.status = (uint8_t) ((readVal >> 2) & 0x1);
+    packet.payload = (uint16_t) ((readVal >> 3) & 0xFFF);
+    packet.bat = (uint8_t) ((readVal >> 15) & 0x7);
+    packet.sensor = (uint8_t) ((readVal >> 18) & 0x7);
+    packet.longAddr = (uint8_t) ((readVal >> 21) & 0xFF);
+    packet.shortAddr = (uint8_t) ((readVal >> 29) & 0x3);
+    packet.addrMode = (uint8_t) ((readVal >> 31) & 0x1);
+
+    // Printing on the console
+    printf("crc             :%x\n", packet.crc);
+    printf("status          :%x\n", packet.status);
+    printf("payload         :%x\n", packet.payload);
+    printf("bat             :%x\n", packet.bat);
+    printf("sensor          :%x\n", packet.sensor);
+    printf("longAddr        :%x\n", packet.longAddr);
+    printf("shortAddr       :%x\n", packet.shortAddr);
+    printf("addrMode        :%x\n", packet.addrMode);
+
+    printf("size of struct packet: %ld\n", sizeof(packet));
+
+
+    return 0;
+}
